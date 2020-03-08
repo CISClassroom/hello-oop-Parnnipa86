a56dc47f86938ea062679a4b26ce526d4f559368
พรรณนิภา ผลเจริญ <58881202+Parnnipa86@users.noreply.github.com>
2020-03-08 23:10:58 +0700

    s

--- untitled/src/com/company/Main.java
+++ untitled/src/com/company/Main.java
@@ -2,32 +2,8 @@ package com.company;
 
 public class Main {
     public void main(String[] arg) {
-        Student noon = new Student();
-
-        noon.name = "Parnnipa Pohnjareon";
-        noon.id = "623410007-0";
-        noon.major = "CIS";
-
-        System.out.println("1." + noon.name + " " + noon.major);
-
-        Student[] oopStudents = {noon,};
-
-        System.out.println("1." + oopStudents[0].name);
-
-        find_CIS_student(oopStudents);
-    }
-
-    private void find_CIS_student(Student[] oopStudents) {
-    }
-
-    public void find_CIS_Student(Student[] students) {
-        for (int i = 0; i < students.length; i++) {
-            // check student.major is "CIS"
-            if (students[i].major.equals("CIS")) {
-                System.out.println(i + 1 + ". " + students[i].name);
-            }
-        }
-
-
+        System.out.println("Hello OOP");
+        Student detail = new Student();
+        detail.GetStudent();
     }
 }
--- untitled/src/com/company/Student.java
+++ untitled/src/com/company/Student.java
@@ -1,8 +1,15 @@
 package com.company;
 
 public class Student {
-    public String name;
-    public String id;
-    public String major;
+    String name = "นางสาวพรรณนิภา ผลเจริญ";
+    String id = "623410007-0";
+    int age = 2563-2544;
+    public void GetStudent()
+    {
+        System.out.println("ชื่อ-สกุล : " + name);
+        System.out.println("รหัสนักศึกษา : " + id);
+        System.out.println("อายุ " + age + " ปี");
+        System.out.println("สาขาวิทยาการคอมพิวเตอร์และสารสนเทศ");
+    }
 
     }
